5e78832e5cf0fb001176da5a0f70af463bf97824
Votre Nom <thibaud>
2023-11-18 21:13:01 +0100

    added somethink workfull

--- get_next_line.c
+++ get_next_line.c
@@ -3,35 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmouche <tmouche@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tmouche < tmouche@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:04:05 by tmouche           #+#    #+#             */
-/*   Updated: 2023/11/16 14:40:56 by tmouche          ###   ########.fr       */
+/*   Updated: 2023/11/18 21:10:10 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-# include <fcntl.h>
+#include <fcntl.h>
+
+char	*ft_set_line(char *str, size_t index)
+{
+	char	*line;
+
+	line = calloc(1, index + 2);
+	if (!line)
+		return (NULL);
+	index = 0;
+	while (str[index])
+	{
+		line[index] = str[index];
+		index++;
+	}
+	return (line);
+
+}
 
 char	*get_next_line(int fd)
 {
-	int	temp_fd;
-	char	*line;
-	char	c;
-	size_t	size;
+	char	*tempo;
+	size_t	index;
 
-	temp_fd = fd;
-	size = 1;
-	while (read(fd, &c, 1) != '\n')
-		size++;
-	if (read(fd, &c, 1) != 0)
-		return (1);
-	line = malloc(sizeof(char) * size + 1);
-	if (!line)
-		return (NULL)
-	
-	
-
+	tempo = calloc(1, BUFFER_SIZE);
+	if (!tempo)
+		return (NULL);
+	index = 0;
+	read(fd, &tempo[index], 1);
+	while (index < BUFFER_SIZE && tempo[index] != '\n' && tempo[index] != 0)
+	{
+		index++;
+		read(fd, &tempo[index], 1);
+	}
+	if (index == BUFFER_SIZE && tempo[index] != '\n')
+		return (ft_set_line(tempo, index));
+	return (tempo);
 }
 
 int	main(int argc, char **argv)
@@ -41,7 +57,10 @@ int	main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
-	get_next_line(fd);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	close(fd);
 	write(1, "\n", 1);
 	return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmouche <tmouche@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tmouche < tmouche@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:06:42 by tmouche           #+#    #+#             */
-/*   Updated: 2023/11/16 14:19:42 by tmouche          ###   ########.fr       */
+/*   Updated: 2023/11/18 20:39:01 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 500
+# endif
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
