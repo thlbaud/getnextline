8b6d08576c769f89d8a2b3bc6d92014882f9c786
Votre Nom <thibaud>
2023-11-18 21:17:03 +0100

    added somethink workfull

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmouche < tmouche@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:04:05 by tmouche           #+#    #+#             */
-/*   Updated: 2023/11/18 21:10:10 by tmouche          ###   ########.fr       */
+/*   Updated: 2023/11/18 21:16:40 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,17 @@ char	*ft_set_line(char *str, size_t index)
 
 	line = calloc(1, index + 2);
 	if (!line)
+	{
+		free (str);
 		return (NULL);
+	}
 	index = 0;
 	while (str[index])
 	{
 		line[index] = str[index];
 		index++;
 	}
+	free (str);
 	return (line);
 
 }
@@ -53,14 +57,14 @@ char	*get_next_line(int fd)
 int	main(int argc, char **argv)
 {
 	int		fd;
+	size_t	i;
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
+	i = 32;
+	while (--i)
+		printf("%s", get_next_line(fd));
 	close(fd);
 	write(1, "\n", 1);
 	return (0);
